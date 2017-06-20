d1ee6f4dacc69d2429e6bce23c0e8a0d6883c022
Sebastian Springer <info@sebastian-springer.com>
2017-06-20 15:11:40 +0200

    aufgabe 7

--- express/index.js
+++ express/index.js
@@ -1,1 +1,17 @@
-const express = require('express'); // constant
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Client');
+});
+app.get('/user', (req, res) => {
+  res.send('Hello User');
+});
+app.get('/user/:id', (req, res) => {
+  res.send('Hello User' + req.params.id);
+});
+
+app.listen(8081, () => {
+  console.log('listening');
+});
