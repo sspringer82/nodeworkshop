d2e8ece0b0d31a39d97d471a71ea94bf3888375d
Sebastian Springer <info@sebastian-springer.com>
2017-06-20 15:23:01 +0200

    express

--- express/index.js
+++ express/index.js
@@ -5,12 +5,6 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello Client');
 });
-app.get('/user', (req, res) => {
-  res.send('Hello User');
-});
-app.get('/user/:id', (req, res) => {
-  res.send('Hello User' + req.params.id);
-});
 
 app.listen(8081, () => {
   console.log('listening');
