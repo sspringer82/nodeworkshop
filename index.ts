6f7b3cfd37a2ff934a617ad900344ab1bd0cd5fb
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-06-11 12:06:58 +0200

    task 1 logging

--- index.ts
+++ index.ts
@@ -2,7 +2,20 @@ import express from 'express';
 
 const app = express();
 
+app.use((request, response, next) => {
+  console.log('middleware');
+
+  next();
+});
+
+app.use((request, response, next) => {
+  console.log('middleware1');
+
+  next();
+});
+
 app.get('/address', (request, response) => {
+  console.log('routing');
   response.json({
     id: 1,
     firstname: 'klaus',
@@ -11,4 +24,6 @@ app.get('/address', (request, response) => {
   });
 });
 
-app.listen(8080);
+app.listen(8080, () =>
+  console.log('Server is running on http://localhost:8080'),
+);
---
+++ tasks/task1.md
@@ -0,0 +1,7 @@
+# Task 1
+
+Implement a routing middlware that logs the following information to the conosle:
+
+- current time
+- requested url
+- request method
